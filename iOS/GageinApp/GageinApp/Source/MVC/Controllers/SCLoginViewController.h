0bb237ccc3a5654ac57d1d3a0a3576fe407ee9dd
Daniel <dymx101@hotmail.com>
2013-03-23 15:20:22 +0800

    Modify Project structure

---
+++ iOS/GageinApp/GageinApp/Source/MVC/Controllers/SCLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCLoginViewController.h
+//  GageinApp
+//
+//  Created by dong yiming on 13-3-23.
+//  Copyright (c) 2013年 gagein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCLoginViewController : UIViewController
+
+@end
