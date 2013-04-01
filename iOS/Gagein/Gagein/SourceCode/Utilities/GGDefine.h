4581a3e7d78470beff575a582880c4cb046870f2
Daniel Dong <dymx101@hotmail.com>
2013-04-01 14:06:14 +0800

    add Debug log macro

--- iOS/Gagein/Gagein/SourceCode/Utilities/GGDefine.h
+++ iOS/Gagein/Gagein/SourceCode/Utilities/GGDefine.h
@@ -8,29 +8,58 @@
 
 #import <Foundation/Foundation.h>
 
+//
 #define GGN_STR_API_BASE_URL    @"https://www.gagein.com/svc/"
 
-
+//
 #undef	__INT
 #define __INT( __x )			[NSNumber numberWithInt:(NSInteger)(__x)]
-
 #undef	__UINT
 #define __UINT( __x )			[NSNumber numberWithUnsignedInt:(NSUInteger)(__x)]
-
 #undef	__LONG
 #define __LONG( __x )			[NSNumber numberWithLong:(long)(__x)]
-
 #undef	__LONGLONG
 #define __LONGLONG( __x )			[NSNumber numberWithLongLong:(long long)(__x)]
-
 #undef	__FLOAT
 #define	__FLOAT( __x )			[NSNumber numberWithFloat:(float)(__x)]
-
 #undef	__DOUBLE
 #define	__DOUBLE( __x )			[NSNumber numberWithDouble:(double)(__x)]
-
 #undef	__BOOL
 #define	__BOOL( __x )			[NSNumber numberWithBool:(BOOL)(__x)]
 
+// 判断是否为IPAD
+#define ISIPADDEVICE (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+
+// 单例
+#undef	AS_SINGLETON
+#define AS_SINGLETON( __class ) \
++ (__class *)sharedInstance;
+
+#undef	DEF_SINGLETON
+#define DEF_SINGLETON( __class ) \
++ (__class *)sharedInstance \
+{ \
+static dispatch_once_t once; \
+static __class * __singleton__; \
+dispatch_once( &once, ^{ __singleton__ = [[__class alloc] init]; } ); \
+return __singleton__; \
+}
+
+//LOG
+#ifdef DEBUG
+#define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+#else
+#define DLog(...)
+#endif
+
+#define ALog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+
+#ifdef DEBUG
+#define DALog(fmt, ...)  { UIAlertView *alert = [[OTSAlertView alloc] initWithTitle:[NSString stringWithFormat:@"%s\n [Line %d] ", __PRETTY_FUNCTION__, __LINE__] message:[NSString stringWithFormat:fmt, ##__VA_ARGS__]  delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil]; [alert show]; }
+#else
+#define DALog(...)
+#endif
+
+//
 @interface GGDefine : NSObject
 @end
